1d3ec5c5c8d7708cf67d0c4b3004871f360a9f4e
tim <tim@localhost>
2012-12-08 14:47:02 +0100

    a setting

---
+++ src/com/googlecode/npackdweb/Setting.java
@@ -0,0 +1,44 @@
+package com.googlecode.npackdweb;
+
+import javax.persistence.Id;
+import javax.persistence.PostLoad;
+import javax.persistence.PrePersist;
+
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.annotation.Cached;
+import com.googlecode.objectify.annotation.Entity;
+
+/**
+ * A setting.
+ */
+@Entity
+@Cached
+public class Setting {
+	/** name of the setting */
+	@Id
+	public String name = "";
+
+	/** value of the setting */
+	public String value = "";
+
+	/**
+	 * For Objectify.
+	 */
+	public Setting() {
+	}
+
+	@PostLoad
+	public void postLoad() {
+	}
+
+	@PrePersist
+	void onPersist() {
+	}
+
+	/**
+	 * @return created Key for this object
+	 */
+	public Key<Setting> createKey() {
+		return new Key<Setting>(Setting.class, this.name);
+	}
+}
