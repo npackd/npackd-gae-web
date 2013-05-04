1e51f4f3e11de82f9e956255ecef1ba4edc86e17
tim <tim@localhost>
2013-05-04 19:23:18 +0200

    create menu in code

--- src/com/googlecode/npackdweb/MyPage.java
+++ src/com/googlecode/npackdweb/MyPage.java
@@ -6,6 +6,7 @@ import java.io.Writer;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.googlecode.npackdweb.wlib.HTMLWriter;
 import com.googlecode.npackdweb.wlib.Page;
 
 /**
@@ -20,7 +21,8 @@ public abstract class MyPage extends Page {
 
         out.write(NWUtils.tmpl("Frame.html", "title", getTitle(), "content",
                 createContent(request), "login",
-                NWUtils.getLoginFooter(request), "head", getHeadPart()));
+                NWUtils.getLoginFooter(request), "head", getHeadPart(), "menu",
+                createMenu()));
         out.close();
     }
 
@@ -60,5 +62,54 @@ public abstract class MyPage extends Page {
      *            HTTP request
      */
     public void fill(HttpServletRequest req) {
+    }
+
+    private String createMenu() {
+        // http://www.red-team-design.com/css3-dropdown-menu
+        HTMLWriter w = new HTMLWriter();
+        w.start("ul", "id", "menu");
+        w.start("li");
+        w.e("a", "href", "/", "Home");
+        w.end("li");
+        w.start("li");
+        w.e("a", "href", "/p", "Packages");
+        w.start("ul");
+        mi(w, "/p", "All packages");
+        mi(w, "/p?q=&sort=created", "All packages sorted by creation date");
+        mi(w,
+                "http://code.google.com/p/windows-package-manager/issues/entry?template=Defect%20report%20from%20user",
+                "Suggest a package*");
+        w.end("ul");
+        w.end("li");
+        w.start("li");
+        w.e("a", "href", "/rep", "Repositories");
+        w.end("li");
+        w.start("li");
+        w.e("a",
+                "href",
+                "http://code.google.com/p/windows-package-manager/downloads/list",
+                "Download client*");
+        w.end("li");
+        w.start("li");
+        w.e("a", "href", "#", "Edit");
+        w.start("ul");
+        mi(w, "/rep/from-file", "Upload from a file");
+        mi(w, "/rep/edit-as-xml", "Upload as text");
+        mi(w, "/package/new", "Create new package");
+        /*<!-- 
+        <li><a href="/add-editor">Add editor</a></li>
+        <li><a href="/recreate-index">Recreate index</a></li>
+        <li><a href="/resave-packages">Re-save packages</a></li>
+         -->*/
+        w.end("ul");
+        w.end("li");
+        w.end("ul");
+        return w.toString();
+    }
+
+    private void mi(HTMLWriter w, String href, String title) {
+        w.start("li");
+        w.e("a", "href", href, title);
+        w.end("li");
     }
 }
--- src/com/googlecode/npackdweb/Repository.java
+++ src/com/googlecode/npackdweb/Repository.java
@@ -14,32 +14,32 @@ import com.googlecode.objectify.annotation.Entity;
  */
 @Entity
 public class Repository {
-	/** name of the repository */
-	@Id
-	public String name;
+    /** name of the repository */
+    @Id
+    public String name;
 
-	/** last modification date */
-	public Date lastModifiedAt;
+    /** last modification date */
+    public Date lastModifiedAt;
 
-	/** path to the XML blob or null */
-	public String blobFile;
+    /** path to the XML blob or null */
+    public String blobFile;
 
-	@PostLoad
-	public void postLoad() {
-		if (this.lastModifiedAt == null)
-			this.lastModifiedAt = new Date();
-	}
+    @PostLoad
+    public void postLoad() {
+        if (this.lastModifiedAt == null)
+            this.lastModifiedAt = new Date();
+    }
 
-	@PrePersist
-	void onPersist() {
-		NWUtils.incDataVersion();
-		this.lastModifiedAt = new Date();
-	}
+    @PrePersist
+    void onPersist() {
+        NWUtils.incDataVersion();
+        this.lastModifiedAt = new Date();
+    }
 
-	/**
-	 * @return created Key for this object
-	 */
-	public Key<Repository> createKey() {
-		return new Key<Repository>(Repository.class, name);
-	}
+    /**
+     * @return created Key for this object
+     */
+    public Key<Repository> createKey() {
+        return new Key<Repository>(Repository.class, name);
+    }
 }
