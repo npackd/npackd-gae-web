027c74fbd234a069cb1d1e5fe23b563ecbd3dde6
tim <tim@localhost>
2013-05-26 10:46:18 +0200

    show all defined repositories (read them from the datastore)

--- src/com/googlecode/npackdweb/RepPage.java
+++ src/com/googlecode/npackdweb/RepPage.java
@@ -1,44 +1,44 @@
 package com.googlecode.npackdweb;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
 import com.googlecode.npackdweb.wlib.HTMLWriter;
+import com.googlecode.objectify.Objectify;
 
 /**
  * List of repositories.
  */
 public class RepPage extends MyPage {
-	private static final String[] TITLES = { "Stable", "Stable 64 bit", "Libs",
-	        "Unstable" };
-	private static final String[] NAMES = { "stable", "stable64", "libs",
-	        "unstable" };
+    @Override
+    public String createContent(HttpServletRequest request) throws IOException {
+        HTMLWriter b = new HTMLWriter();
 
-	@Override
-	public String createContent(HttpServletRequest request) throws IOException {
-		HTMLWriter b = new HTMLWriter();
+        b.t("These repositories are re-created daily:");
+        b.start("ul");
+        Objectify ofy = NWUtils.getObjectify();
+        List<Repository> reps = Repository.findAll(ofy);
+        for (int i = 0; i < reps.size(); i++) {
+            Repository r = reps.get(i);
 
-		b.t("These repositories are re-created daily:");
-		b.start("ul");
-		for (int i = 0; i < TITLES.length; i++) {
-			b.start("li");
-			b.e("a", "href", "/rep/xml?tag=" + NAMES[i], TITLES[i]);
-			b.end("li");
-		}
-		b.end("ul");
+            b.start("li");
+            b.e("a", "href", "/rep/xml?tag=" + r.name, r.name);
+            b.end("li");
+        }
+        b.end("ul");
 
-		b.e("br");
-		b
-		        .t("This repository contains 20 last changed package versions and should be used for testing only: ");
-		b.e("a", "href", "/rep/recent-xml",
-		        "20 recently modified package versions");
+        b.e("br");
+        b.t("This repository contains 20 last changed package versions and should be used for testing only: ");
+        b.e("a", "href", "/rep/recent-xml",
+                "20 recently modified package versions");
 
-		return b.toString();
-	}
+        return b.toString();
+    }
 
-	@Override
-	public String getTitle() {
-		return "Repositories";
-	}
+    @Override
+    public String getTitle() {
+        return "Repositories";
+    }
 }
