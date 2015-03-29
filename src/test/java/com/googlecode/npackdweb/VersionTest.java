c79e9f0a1aecdbc5fc369938ca8c90566b249e0d
Tim Lebedkov <tim.lebedkov@gmail.com>
2015-03-29 15:49:10 +0200

    Netbeans

---
+++ src/test/java/com/googlecode/npackdweb/VersionTest.java
@@ -0,0 +1,37 @@
+package com.googlecode.npackdweb;
+
+import com.googlecode.npackdweb.Version;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Tests for the Version class.
+ */
+public class VersionTest {
+	@Test
+	/**
+	 * Tests the normalize() method
+	 */
+	public void testNormalize() {
+		Version v = Version.parse("1.0");
+		v.normalize();
+		assertEquals("1", v.toString());
+
+		v = Version.parse("123.0.0.0");
+		v.normalize();
+		assertEquals("123", v.toString());
+
+		v = Version.parse("17.2.0.0");
+		v.normalize();
+		assertEquals("17.2", v.toString());
+
+		v = Version.parse("178");
+		v.normalize();
+		assertEquals("178", v.toString());
+
+		v = Version.parse("178.12");
+		v.normalize();
+		assertEquals("178.12", v.toString());
+	}
+}
