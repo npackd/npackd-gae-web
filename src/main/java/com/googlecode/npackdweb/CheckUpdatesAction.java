ec2c113aa14c5947c45b2e424b6818c7ab1cf920
Tim Lebedkov <tim.lebedkov@gmail.com>
2017-06-30 21:05:28 +0200

    automatically create versions for all packages

--- src/main/java/com/googlecode/npackdweb/CheckUpdatesAction.java
+++ src/main/java/com/googlecode/npackdweb/CheckUpdatesAction.java
@@ -65,7 +65,7 @@ public class CheckUpdatesAction extends Action {
                         if (r == 0) {
                             noUpdatesCheck = NWUtils.newDate();
                         } else if (r > 0) {
-                            if (old.hasTag("auto-create-versions")) {
+                            /*if (old.hasTag("auto-create-versions"))*/ {
                                 final PackageVersion d =
                                         data.createDetectedVersion(ob, v);
                                 if (d != null) {
