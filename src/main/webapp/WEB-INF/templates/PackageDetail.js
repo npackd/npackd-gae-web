dbde7ab418ed30d13f1f8bbeb84b985e9ce7c73d
tim-lebedkov <tim.lebedkov@gmail.com>
2017-10-09 21:30:06 +0200

    Most often used reason

--- src/main/webapp/WEB-INF/templates/PackageDetail.js
+++ src/main/webapp/WEB-INF/templates/PackageDetail.js
@@ -52,7 +52,7 @@ function updateTagInput() {
 
 function deleteOnClick() {
 	var msg = prompt("Deleting the package. Please enter the explanations.", 
-			"See https://github.com/tim-lebedkov/npackd/wiki/RejectedSoftware for more details.");
+			"Seems to be just a test.");
 	if (msg !== null) {
 		var id = $('#name').val();
 		window.location.href='/package/delete-confirmed?name=' + id + 
