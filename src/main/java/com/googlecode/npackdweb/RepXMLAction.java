41d0927cc29ad9244225c556eeb8986b5d945888
tim-lebedkov <tim.lebedkov@gmail.com>
2018-02-19 20:32:14 +0100

    Avoid server side errors if the supplied repository name is empty (/rep/xml|zip)

--- src/main/java/com/googlecode/npackdweb/RepXMLAction.java
+++ src/main/java/com/googlecode/npackdweb/RepXMLAction.java
@@ -1,20 +1,19 @@
 package com.googlecode.npackdweb;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.googlecode.npackdweb.db.Repository;
 import com.googlecode.npackdweb.wlib.Action;
 import com.googlecode.npackdweb.wlib.ActionSecurityType;
 import com.googlecode.npackdweb.wlib.Page;
 import com.googlecode.objectify.Objectify;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Creates XML for a whole repository definition.
  */
 public class RepXMLAction extends Action {
+
     /**
      * -
      */
@@ -27,14 +26,20 @@ public class RepXMLAction extends Action {
             throws IOException {
         String tag = req.getParameter("tag");
 
-        Objectify ofy = DefaultServlet.getObjectify();
-        Repository r = Repository.findByTag(ofy, tag);
-        if (r == null) {
-            resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Repository "
-                    + tag + " not found");
-            return null;
+        if (tag != null && !tag.isEmpty()) {
+            Objectify ofy = DefaultServlet.getObjectify();
+            Repository r = Repository.findByTag(ofy, tag);
+            if (r == null) {
+                resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Repository " +
+                        tag + " not found");
+                return null;
+            } else {
+                return new RepXMLPage(tag);
+            }
         } else {
-            return new RepXMLPage(tag);
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST,
+                    "Empty repository name");
+            return null;
         }
     }
 }
--- src/main/java/com/googlecode/npackdweb/RepZIPAction.java
+++ src/main/java/com/googlecode/npackdweb/RepZIPAction.java
@@ -1,40 +1,45 @@
 package com.googlecode.npackdweb;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.googlecode.npackdweb.db.Repository;
 import com.googlecode.npackdweb.wlib.Action;
 import com.googlecode.npackdweb.wlib.ActionSecurityType;
 import com.googlecode.npackdweb.wlib.Page;
 import com.googlecode.objectify.Objectify;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Creates ZIP for a whole repository definition.
  */
 public class RepZIPAction extends Action {
-	/**
-	 * -
-	 */
-	public RepZIPAction() {
-		super("^/rep/zip$", ActionSecurityType.ANONYMOUS);
-	}
 
-	@Override
-	public Page perform(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-		String tag = req.getParameter("tag");
+    /**
+     * -
+     */
+    public RepZIPAction() {
+        super("^/rep/zip$", ActionSecurityType.ANONYMOUS);
+    }
 
-		Objectify ofy = DefaultServlet.getObjectify();
-		Repository r = Repository.findByTag(ofy, tag);
-		if (r == null) {
-			resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Repository " +
-					tag + " not found");
-			return null;
-		} else {
-			return new RepZIPPage(tag);
-		}
-	}
+    @Override
+    public Page perform(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        String tag = req.getParameter("tag");
+
+        if (tag != null && !tag.isEmpty()) {
+            Objectify ofy = DefaultServlet.getObjectify();
+            Repository r = Repository.findByTag(ofy, tag);
+            if (r == null) {
+                resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Repository " +
+                        tag + " not found");
+                return null;
+            } else {
+                return new RepZIPPage(tag);
+            }
+        } else {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST,
+                    "Empty repository name");
+            return null;
+        }
+    }
 }
