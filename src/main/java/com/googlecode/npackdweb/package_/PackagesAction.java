449a50b4503aee99451ae823587832fb98a89eeb
tim-lebedkov <tim.lebedkov@gmail.com>
2018-07-17 20:36:16 +0200

    Bugfix: show an error message for offsets > 1000

--- src/main/java/com/googlecode/npackdweb/package_/PackagesAction.java
+++ src/main/java/com/googlecode/npackdweb/package_/PackagesAction.java
@@ -1,43 +1,52 @@
 package com.googlecode.npackdweb.package_;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.googlecode.npackdweb.MessagePage;
 import com.googlecode.npackdweb.wlib.Action;
 import com.googlecode.npackdweb.wlib.ActionSecurityType;
 import com.googlecode.npackdweb.wlib.Page;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Packages
  */
 public class PackagesAction extends Action {
-	/**
-	 * -
-	 */
-	public PackagesAction() {
-		super("^/p$", ActionSecurityType.ANONYMOUS);
-	}
 
-	@Override
-	public Page perform(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-		String start_ = req.getParameter("start");
-		if (start_ == null)
-			start_ = "0";
-		int start;
-		try {
-			start = Integer.parseInt(start_);
-		} catch (NumberFormatException e) {
-			start = 0;
-		}
-		String q = req.getParameter("q");
-		if (q == null)
-			q = "";
+    /**
+     * -
+     */
+    public PackagesAction() {
+        super("^/p$", ActionSecurityType.ANONYMOUS);
+    }
 
-		return new PackagesPage(q, "created".equals(req.getParameter("sort")),
-				start, req.getParameter("category0"),
-				req.getParameter("category1"));
-	}
+    @Override
+    public Page perform(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        String start_ = req.getParameter("start");
+        if (start_ == null) {
+            start_ = "0";
+        }
+        int start;
+        try {
+            start = Integer.parseInt(start_);
+        } catch (NumberFormatException e) {
+            start = 0;
+        }
+
+        if (start >= 1000) {
+            return new MessagePage(
+                    "Cannot show results after 1000. Try to filter by a category first.");
+        } else {
+            String q = req.getParameter("q");
+            if (q == null) {
+                q = "";
+            }
+
+            return new PackagesPage(q, "created".
+                    equals(req.getParameter("sort")),
+                    start, req.getParameter("category0"),
+                    req.getParameter("category1"));
+        }
+    }
 }
